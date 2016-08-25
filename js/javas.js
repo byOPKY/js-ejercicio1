281b18b159092291dde607d2d65434c3b5d609b1
byOPKY <orrillokarol@gmail.com>
2016-08-25 14:58:22 -0500

    ejercicio1

---
+++ js/javas.js
@@ -0,0 +1,13 @@
+window.alert("Calcula tu suma");
+var num1 =parseInt(prompt ("Ingrese un numero"));
+var num2 =parseInt(prompt ("Ingrese un numero"));
+var suma = num1 + num2;
+document.write("Total de tu suma es " + suma);
+
+
+
+
+
+
+
+
